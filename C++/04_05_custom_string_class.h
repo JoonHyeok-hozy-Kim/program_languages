ec7faef6702bae084a70bb880c9f366e6d6794fa
HOzy-Joon <danielisdan@naver.com>
2022-08-11 00:47:58 +0900

    MyString Class migrated from cpp to h

--- C++/04_05_custom_string_class.h
+++ C++/04_05_custom_string_class.h
@@ -1,3 +1,39 @@
 
 
+class MyString {
+	char* string_content;
+	int string_length;
+	int capacity;
+
+public:
+	MyString(char c);
+	MyString(const char* str);
+	MyString(const MyString& str);
+	~MyString() { delete[] string_content; }
+	int length() const { return string_length; }
+	int show_capacity() const { return capacity; }
+	void print() const;
+	void println() const;
+	void capacity_change(int new_capacity, int prev_capacity);
+	void capacity_check(int target_capacity);
+	MyString& assign(const char* str);
+	MyString& assign(const MyString& str);
+	char at(int i) const;
+
+	MyString& insert(int loc, MyString& str);
+	MyString& insert(int loc, const char* str);
+	MyString& insert(int loc, char c);
+	MyString& erase(int loc, int num);
+
+	int find(int start, const MyString& str) const;
+	int find(int start, const char* str) const;
+	int find(int start, char c) const;
+	int find_brute_force(int start, const MyString& str) const;
+	int find_kmp(int start, const MyString& str) const;
+	int* _compute_kmp_fail(const MyString& str) const;
+
+	int compare(const MyString& str) const;
+
+};
+
 void mystring_instance_test();
